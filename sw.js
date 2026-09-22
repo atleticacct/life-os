26d5263bdcfe329ff26367247676a1894ece0dcb
atleticacct <appaaacct@gmail.com>
2026-09-22 19:52:30 -0300

    Simplifica tela inicial para limites por categoria V1.0.30

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lifeos-v1.0.29-pwa-1';
+const CACHE='lifeos-v1.0.30-pwa-1';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
